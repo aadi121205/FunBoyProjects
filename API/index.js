c39ca66ca33aae2cd9c45a9c611e7756447bda1f
Aadi <aaditya12120205@gmail.com>
2025-03-29 06:12:29 +0530

    update

--- API/index.js
+++ API/index.js
@@ -2,19 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 7000;
-// db.js
-const { Pool } = require('pg');
-require('dotenv').config(); // Optional, if using .env
 
-const pool = new Pool({
-  user: process.env.DB_USER || 'your_user',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'your_db',
-  password: process.env.DB_PASSWORD || 'your_password',
-  port: process.env.DB_PORT || 5432,
-});
-
-module.exports = pool;
 
 app.get('/api', (req, res) => {
   res.send('API is working');
